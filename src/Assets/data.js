de07c3c3840b354c6b91d8f733c5150dbf6d3e72
preet <184svgms@gmail.com>
2025-02-27 21:16:57 +0530

    first commit

---
+++ src/Assets/data.js
@@ -0,0 +1,283 @@
+export const data = [
+    {
+        id:"1",
+        name:"Mens Mock Neck Short Sleeve T-Shirts",
+        image:"https://i.pinimg.com/736x/b7/45/58/b7455851debf1e80d3e14b1393244940.jpg",
+        price:199,
+        category:"men"
+    },
+    {
+        id:"2",
+        name:"Men's Printed Casual Shirt",
+        image:"https://i.pinimg.com/736x/6d/5c/51/6d5c51a841f0196af85c59325e5b52c1.jpg",
+        price:399,
+        category:"men"
+    },
+    {
+        id:"3",
+        name:"Men's Slim Fit Black Stretch Jeans",
+        image:"https://i.pinimg.com/736x/ce/fc/1f/cefc1f2975b21dbf0f8df45681c87b55.jpg",
+        price:799,
+        category:"men"
+    },
+    {
+        id:"4",
+        name:"boAt Rockerz450 Bluetooth On Ear Headphones with Mic",
+        image:"https://m.media-amazon.com/images/I/51xxA+6E+xL._SX522_.jpg",
+        price:1999,
+        category:"electric"
+    },
+    {
+        id:"5",
+        name:"Cuban Silver Bracelet for Men Boys",
+        image:"https://m.media-amazon.com/images/I/512hvqlxOuL._UY625_.jpg",
+        price:2199,
+        category:"jewellery"
+    },
+    {
+        id:"6",
+        name:"Men's Round Neck Cotton Blend Black T-Shirt",
+        image:"https://i.pinimg.com/736x/55/14/69/5514694b23da5777214fda088b3d57fb.jpg",
+        price:199,
+        category:"men"
+    },
+    {
+        id:"7",
+        name:"Men's Printed Casual Shirt",
+        image:"https://rukminim1.flixcart.com/image/832/832/l2xmqvk0/shirt/z/i/x/m-ad-egypt-shirt-02-ak-impex-original-image5xzxaazchh8.jpeg?q=70",
+        price:399,
+        category:"men"
+    },
+    {
+        id:"8",
+        name:"Men's Slim Fit Black Stretch Jeans",
+        image:"https://m.media-amazon.com/images/I/61VYhbu1a-L._UX466_.jpg",
+        price:799,
+        category:"men"
+    },
+    {
+        id:"9",
+        name:"boAt Rockerz450 Bluetooth On Ear Headphones with Mic",
+        image:"https://m.media-amazon.com/images/I/51xxA+6E+xL._SX522_.jpg",
+        price:1999,
+        category:"electric"
+    },
+    {
+        id:"10",
+        name:"Cuban Silver Bracelet for Men Boys",
+        image:"https://m.media-amazon.com/images/I/512hvqlxOuL._UY625_.jpg",
+        price:2199,
+        category:"jewellery"
+    },
+    {
+        id:"11",
+        name:"Laptop Backpack with USB Charging Port | Multi functional Backpack (Grey)",
+        image:"https://i.pinimg.com/736x/eb/c2/4a/ebc24a1011ea299fe3d9b448aebfb817.jpg",
+        price:1099,
+        category:"men"
+    },
+    {
+        id:"12",
+        name:"Noise Twist Bluetooth Calling Smart Watch",
+        image:"https://m.media-amazon.com/images/I/61TapeOXotL._SX679_.jpg",
+        price:2199,
+        category:"electric"
+    },
+    {
+        id:"13",
+        name:"HP All-in-One Desktop PC (8GB/256GB SSD + 1TB HDD/FHD)",
+        image:"https://m.media-amazon.com/images/I/61ogPth2lzL._SX679_.jpg",
+        price:45999,
+        category:"electric"
+    },
+    {
+        id:"14",
+        name:"Women's Hoodies",
+        image:"https://m.media-amazon.com/images/I/51CbQi8QLzL._UX466_.jpg",
+        price:2999,
+        category:"women"
+    },
+    {
+        id:"15",
+        name:"Women's Sweatshirt 'M' size Pink",
+        image:"https://m.media-amazon.com/images/I/71eq6q3uVeL._AC_UY1100_.jpg",
+        price:3499,
+        category:"women"
+    },
+    {
+        id:"16",
+        name:"Gold Jewellery Set Necklace and Earrings",
+        image:"https://m.media-amazon.com/images/I/41L6gVqjhQL._AC_UL400_.jpg",
+        price:25499,
+        category:"jewellery"
+    },
+    {
+        id:"17",
+        name:"Women Leather Bag",
+        image:"https://m.media-amazon.com/images/I/71Y32csE9vL._UY695_.jpg",
+        price:4449,
+        category:"women"
+    },
+    {
+        id:"18",
+        name:"Laptop Backpack with USB Charging Port | Multi functional Backpack (Grey)",
+        image:"https://m.media-amazon.com/images/I/51rga1GR6rL.jpg",
+        price:1099,
+        category:"men"
+    },
+    {
+        id:"19",
+        name:"Noise Twist Bluetooth Calling Smart Watch",
+        image:"https://m.media-amazon.com/images/I/61TapeOXotL._SX679_.jpg",
+        price:2199,
+        category:"electric"
+    },
+    {
+        id:"20",
+        name:"HP All-in-One Desktop PC (8GB/256GB SSD + 1TB HDD/FHD)",
+        image:"https://m.media-amazon.com/images/I/61ogPth2lzL._SX679_.jpg",
+        price:45999,
+        category:"electric"
+    },
+    {
+        id:"21",
+        name:"Women's Hoodies",
+        image:"https://m.media-amazon.com/images/I/51CbQi8QLzL._UX466_.jpg",
+        price:2999,
+        category:"women"
+    },
+    {
+        id:"22",
+        name:"Women's Sweatshirt 'M' size Pink",
+        image:"https://m.media-amazon.com/images/I/71eq6q3uVeL._AC_UY1100_.jpg",
+        price:3499,
+        category:"women"
+    },
+    {
+        id:"23",
+        name:"Gold Jewellery Set Necklace and Earrings",
+        image:"https://m.media-amazon.com/images/I/41L6gVqjhQL._AC_UL400_.jpg",
+        price:25499,
+        category:"jewellery"
+    },
+    {
+        id:"24",
+        name:"HP All-in-One Desktop PC (8GB/256GB SSD + 1TB HDD/FHD)",
+        image:"https://m.media-amazon.com/images/I/61ogPth2lzL._SX679_.jpg",
+        price:45999,
+        category:"electric"
+    },
+    {
+        id:"25",
+        name:"Women's Hoodies",
+        image:"https://m.media-amazon.com/images/I/51CbQi8QLzL._UX466_.jpg",
+        price:2999,
+        category:"women"
+    },
+    {
+        id:"26",
+        name:"Women's Sweatshirt 'M' size Pink",
+        image:"https://m.media-amazon.com/images/I/71eq6q3uVeL._AC_UY1100_.jpg",
+        price:3499,
+        category:"women"
+    },
+    {
+        id:"27",
+        name:"Gold Jewellery Set Necklace and Earrings",
+        image:"https://m.media-amazon.com/images/I/41L6gVqjhQL._AC_UL400_.jpg",
+        price:25499,
+        category:"jewellery"
+    },
+    {
+        id:"28",
+        name:"Women Leather Bag",
+        image:"https://m.media-amazon.com/images/I/71Y32csE9vL._UY695_.jpg",
+        price:4449,
+        category:"women"
+    },
+    {
+        id:"29",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"30",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"31",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"32",
+        name:"Women's Hoodies",
+        image:"https://m.media-amazon.com/images/I/51CbQi8QLzL._UX466_.jpg",
+        price:2999,
+        category:"women"
+    },
+    {
+        id:"33",
+        name:"Women's Sweatshirt 'M' size Pink",
+        image:"https://m.media-amazon.com/images/I/71eq6q3uVeL._AC_UY1100_.jpg",
+        price:3499,
+        category:"women"
+    },
+    {
+        id:"34",
+        name:"Gold Jewellery Set Necklace and Earrings",
+        image:"https://m.media-amazon.com/images/I/41L6gVqjhQL._AC_UL400_.jpg",
+        price:25499,
+        category:"jewellery"
+    },
+    {
+        id:"35",
+        name:"Women Leather Bag",
+        image:"https://m.media-amazon.com/images/I/71Y32csE9vL._UY695_.jpg",
+        price:4449,
+        category:"women"
+    },
+    {
+        id:"36",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"37",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"38",
+        name:"WildHorn Wallet for Men",
+        image:"https://m.media-amazon.com/images/I/914s7a8twcL._SX679_.jpg",
+        price:649,
+        category:"men"
+    },
+    {
+        id:"39",
+        name:"Gold Jewellery Set Necklace and Earrings",
+        image:"https://m.media-amazon.com/images/I/41L6gVqjhQL._AC_UL400_.jpg",
+        price:25499,
+        category:"jewellery"
+    },
+    {
+        id:"40",
+        name:"HP Wired Keyboard",
+        image:"https://m.media-amazon.com/images/I/31-AngJwVkL._SX300_SY300_QL70_FMwebp_.jpg",
+        price:599,
+        category:"electric"
+    }
+    
+];
